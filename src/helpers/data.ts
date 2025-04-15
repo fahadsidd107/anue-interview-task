1630548a706985786de84094876e7f1446e0a792
margo250494@gmail.com <Margo250494@github.com>
2025-04-15 19:24:57 +0200

    check palindrome: functionality, styles

---
+++ src/helpers/data.ts
@@ -0,0 +1,21 @@
+interface Steps{
+	id: string;
+	title: string;
+}
+
+
+export const intsructionsSteps:Steps[] = [
+	{
+		id: 'step-1',
+		title: '1. Enter a word or phrase in the input field.',
+	},
+	{
+		id: 'step-2',
+		title: '2. Click the "Check" button.',
+	},
+	{
+		id: 'step-3',
+		title: '3. If the input is a palindrome, a message will be displayed below.',
+	},
+	
+]
